42c939a72544d583ca86442b16a218f2e371f4ea
Chris Bechard <cbechar7@uwo.ca>
2019-03-05 16:41:15 -0500

    Edited react build command, VIM serves production build assets

---
+++ VIM/app.py
@@ -0,0 +1,16 @@
+import os
+from flask import Flask, send_from_directory
+
+app = Flask(__name__, static_folder='assets')
+
+# Serve the React App
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def serve(path):
+    if path != "" and os.path.exists("assets/" + path):
+        return send_from_directory('assets', path)
+    else:
+        return send_from_directory('assets', 'index.html')
+
+if __name__ == '__main__':
+    app.run(use_reloader=True, port=5000, threaded=True)
