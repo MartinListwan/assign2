b6943d2a3d2cbfb81d687c5016b41c29654f416c
Chris Bechard <cbechar7@uwo.ca>
2019-03-05 20:26:14 -0500

    Add a plain login html page and a session manager

--- VIM/app.py
+++ VIM/app.py
@@ -1,17 +1,35 @@
 import os
-import json
-from flask import Flask, send_from_directory
+from flask import Flask, send_from_directory, render_template, redirect
+from flask_login import LoginManager, login_required, logout_user
 
-app = Flask(__name__, static_folder='assets')
+app = Flask(__name__, static_folder='build')
+login_manager = LoginManager()
+login_manager.init_app(app)
 
 # Serve the React App
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
+@login_required
 def serve(path):
-    if path != "" and os.path.exists("assets/" + path):
-        return send_from_directory('assets', path)
+    if path != "" and os.path.exists('build/' + path):
+        return send_from_directory('build', path)
     else:
-        return send_from_directory('assets', 'index.html')
+        return send_from_directory('build', 'index.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect('/login')
+
+@login_manager.unauthorized_handler
+def unauthorized():
+    return redirect('/login') 
 
 if __name__ == '__main__':
     app.run(use_reloader=True, port=5000, threaded=True)
+
